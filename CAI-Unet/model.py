e09d100b1b6dac6f236d3f6971d724f7b77efc35
uqdlund <uqdlund@wiener.hpc.dc.uq.edu.au>
2019-09-25 14:21:59 +1000

    Option for residual connections implemented Tested with several depths and dilated blocks

--- CAI-Unet/model.py
+++ CAI-Unet/model.py
@@ -29,20 +29,9 @@ def unet_model_3d(input_shape, strided_conv_size=(2, 2, 2), n_labels=1, initial_
                   batch_normalization=False, activation_name="sigmoid"):
     """
     Builds the 3D UNet Keras model.f
-    :param metrics: List metrics to be calculated during model training (default is dice coefficient).
-    
-    :param include_label_wise_dice_coefficients: If True and n_labels is greater than 1, model will report the dice
-    coefficient for each label as metric.
-    
-    :param n_base_filters: The number of filters that the first layer in the convolution network will have. Following
-    layers will contain a multiple of this number. Lowering this number will likely reduce the amount of memory required
-    to train the model.
-    
-    :param depth: indicates the depth of the U-shape for the model. The greater the depth, the more strided convolutional
-    layers will be added to the model. Lowering the depth may reduce the amount of memory required for training.
     
     :param input_shape: Shape of the input data (n_chanels, x_size, y_size, z_size). The x, y, and z sizes must be
-    divisible by the pool size to the power of the depth of the UNet, that is strided_conv_size^depth.
+    divisible by the (pool size)/(strided_conv_size) to the power of the depth of the UNet, that is strided_conv_size^depth.
     
     :param strided_conv_size: Size for the strided convolution operations
     
@@ -50,8 +39,33 @@ def unet_model_3d(input_shape, strided_conv_size=(2, 2, 2), n_labels=1, initial_
     
     :param initial_learning_rate: Initial learning rate for the model. This will be decayed during training.
     
-    :param deconvolution: If set to True, will use transpose convolution (deconvolution) instead of up-sampling. This
-    increases the amount memory required during training.
+    :param deconvolution: Boolean - If set to True, will use transpose convolution (deconvolution) instead of up-sampling. This
+    increases the amount memory required during training. (default is False)
+    
+    :param dilation_block: Boolean - if set to True, will construct dilated block between encoder-decoder as in https://doi.org/10.3389/fninf.2019.00030 
+    depends on 'n_dil_block' for amount of dilated blocks to implement (default is False)
+    
+    :param n_dil_blocks: Number of dilated blocks to implement - starts from lowest possible layer i.e. depth-1 (default is 1 block)
+    
+    :param residual: Boolean - if set to True, will employ residual connections on each depth-level, currently matching n_filters for strided convolutions in encoder
+    and 1x1x1 convolution to normalize input in decoder (default is False)
+    
+    :param depth: indicates the depth of the U-shape for the model. The greater the depth, the more (pooling layers)/(strided convolutional layers)
+    will be added to the model. Lowering the depth will reduce the amount of memory required for training. (default is 4 (which is also where most testing occurred))
+    
+    :param n_base_filters: Number of filters that the first layer in the convolution network will have. Following layers will contain a 
+    multiple of this number (doubling for each depth-level). Lowering this number will reduce the amount of memory required to train the model. (default is 32)
+    
+    :param include_label_wise_dice_coefficients: If True and n_labels is greater than 1, model will report the dice
+    coefficient for each label as metric. (default is False)
+    
+    :param metrics: List metrics to be calculated during model training (default is dice coefficient).
+    
+    :param batch_normalization: Boolean - If set to true incoorporates batch normalization in every convolutional layer (default is False)
+    
+    :param activation_name: relevant activation function in final layer before inference/prediction
+    
+    
     :return: Untrained 3D UNet Model
     """
     inputs = Input(input_shape)
@@ -62,7 +76,9 @@ def unet_model_3d(input_shape, strided_conv_size=(2, 2, 2), n_labels=1, initial_
     for layer_depth in range(depth):
         layer1 = create_convolution_block(input_layer=current_layer, n_filters=n_base_filters*(2**layer_depth),
                                           batch_normalization=batch_normalization)
+        ## Residual implementation ##
         if residual and layer_depth>0:
+            
             layer2 = create_convolution_block(input_layer=layer1, n_filters=n_base_filters*(2**layer_depth),
                                           batch_normalization=batch_normalization, act_man = True)
             layer2 = add([layer2, current_layer])
@@ -70,43 +86,59 @@ def unet_model_3d(input_shape, strided_conv_size=(2, 2, 2), n_labels=1, initial_
         else:
             layer2 = create_convolution_block(input_layer=layer1, n_filters=n_base_filters*(2**layer_depth),
                                           batch_normalization=batch_normalization)
-            
+        # for all levels except lowest, create strided convolutional layer   
         if layer_depth < depth - 1:
-            # Create pooling layer --> replaced with strided convolution strides = (2,2,2)
-            current_layer = create_convolution_block(input_layer=layer2, n_filters=n_base_filters*(2**layer_depth),
+            # Create pooling layer --> replaced with strided convolution strides = (2,2,2)##      REMOVE +1 here depending on choice for residual ##
+            current_layer = create_convolution_block(input_layer=layer2, n_filters=n_base_filters*(2**(layer_depth+1)),
                                           batch_normalization=batch_normalization, strides=strided_conv_size) #MaxPooling3D(pool_size=pool_size)(layer2)
             
+            # Keep track of layers in each level for later upsampling
             levels.append([layer1, layer2, current_layer])
+        # For last layer, do ##not## include strided convolution and just have the two normal convolutions
         else:
-            if residual:
-                layer2 = add([layer2, current_layer])
-            else:
-                current_layer = layer2
-            
+            current_layer = layer2
             levels.append([layer1, layer2])
 
-    # add levels with up-convolution (Transposed convolution) or up-sampling
-    # Note, backwards iteration to hit correct layers to concatenate
+    # add levels with up-convolution (Transposed convolution) or up-sampling using 'levels' list
+    # Note, backwards iteration necessary to hit correct layers, in the correct order, for concatenation
     for layer_depth in range(depth-2, -1, -1):
         up_convolution = get_up_convolution(strided_conv_size=strided_conv_size, deconvolution=deconvolution,
                                             n_filters=current_layer._keras_shape[1])(current_layer)
-        # Dilated_fusion_block here # 
-        # Currently only implemented at lowest level, modify for-loop here if wanted on multiple levels, should be permutable, just specify
+        
+        ## Dilated_fusion_block ##
+        # ##Legacy comment## modify for-loop here if wanted on multiple levels, should be permutable, just specify
         # >= depth - (depth-1) for all except top layer, and depth > depth-depth
         if layer_depth >= (depth-(n_dil_block+1)) and dilation_block:
             dil_out = create_dilated_fusion_block(input_layer=levels[layer_depth][1], n_filters=levels[layer_depth][1]._keras_shape[1], layer_depth=layer_depth, dilation_depth=3)
             concat = concatenate([up_convolution, dil_out], axis=1)
+        
         else:
             # Concatenate [layer_depth][1] since [2] is strided convolution/max-pooling
             concat = concatenate([up_convolution, levels[layer_depth][1]], axis=1)        
         
-        current_layer = create_convolution_block(n_filters=levels[layer_depth][1]._keras_shape[1],
+        ## Residual implementation  ##
+        if residual:
+            # 1x1x1 layer to normalize number of feature maps
+            norm_conv = create_convolution_block(n_filters=levels[layer_depth][1]._keras_shape[1],
+                                                 input_layer=concat, 
+                                                 batch_normalization=batch_normalization, 
+                                                 kernel = (1,1,1))
+            current_layer = create_convolution_block(n_filters=levels[layer_depth][1]._keras_shape[1],
+                                                 input_layer=concat, 
+                                                 batch_normalization=batch_normalization)
+            current_layer = create_convolution_block(n_filters=levels[layer_depth][1]._keras_shape[1],
+                                                 input_layer=current_layer,
+                                                 batch_normalization=batch_normalization, act_man=True)
+            current_layer = add([current_layer, norm_conv])
+            current_layer = Activation('relu')(current_layer)
+        
+        else:
+            current_layer = create_convolution_block(n_filters=levels[layer_depth][1]._keras_shape[1],
                                                  input_layer=concat, batch_normalization=batch_normalization)
-        current_layer = create_convolution_block(n_filters=levels[layer_depth][1]._keras_shape[1],
+            current_layer = create_convolution_block(n_filters=levels[layer_depth][1]._keras_shape[1],
                                                  input_layer=current_layer,
                                                  batch_normalization=batch_normalization)
-        if residual:
-            current_layer = add([current_layer, concat])
+
 
     final_convolution = Conv3D(n_labels, (1, 1, 1))(current_layer)
     act = Activation(activation_name)(final_convolution)
@@ -129,17 +161,22 @@ def unet_model_3d(input_shape, strided_conv_size=(2, 2, 2), n_labels=1, initial_
 def create_convolution_block(input_layer, n_filters, batch_normalization=False, kernel=(3, 3, 3), activation=None,
                              padding='same', strides=(1, 1, 1), instance_normalization=False, act_man = False):
     """
-    :param strides:
     :param input_layer:
     :param n_filters:
     :param batch_normalization:
     :param kernel:
     :param activation: Keras activation layer to use. (default is 'relu')
     :param padding:
-    :return:
+    :param strides:
+    :param instance_normalization: Different type of regularization, that can be employed if not wanting batch_normalization
+    :param act_man: Boolean - if residual implementation is wanted, addition must happen before activation, therefore this allows for manual activation
+    
+    :return: A Keras convolution block. Can be normalized (not default) and activated (default)
     """
     if strides!=(1,1,1):
         layer = Conv3D(n_filters, kernel, padding=padding, kernel_initializer='he_normal', strides=strides, name='Strided_conv'+str(input_layer._keras_shape[1]))(input_layer)
+    elif kernel!=(3,3,3):
+        layer = Conv3D(n_filters, kernel, padding=padding, kernel_initializer='he_normal', strides=strides, name='1x1x1'+str(input_layer._keras_shape[1]))(input_layer)
     else:
         layer = Conv3D(n_filters, kernel, padding=padding, kernel_initializer='he_normal', strides=strides)(input_layer)
     if batch_normalization:
@@ -161,7 +198,7 @@ def create_convolution_block(input_layer, n_filters, batch_normalization=False, 
 
 def compute_level_output_shape(n_filters, depth, strided_conv_size, image_shape):
     """
-    Each level has a particular output shape based on the number of filters used in that level and the depth or number 
+    Helper function - Each level has a particular output shape based on the number of filters used in that level and the depth or number 
     of max pooling operations that have been done on the data at that point.
     :param image_shape: shape of the 3d image.
     :param strided_conv_size: the strided_conv_size parameter used in the strided convolution operation.
@@ -175,6 +212,10 @@ def compute_level_output_shape(n_filters, depth, strided_conv_size, image_shape)
 
 def get_up_convolution(n_filters, strided_conv_size, kernel_size=(2, 2, 2), strides=(2, 2, 2),
                        deconvolution=False):
+    '''
+    Construct upsampling block. NOTE does not perform operation as the other functions, just provides the block to do so!
+    :param deconvolution: Boolean - If True use deconvolution (transposed3D convolution), if False use Upsampling (default is False)
+    '''
     if deconvolution:
         return Deconvolution3D(filters=n_filters, kernel_size=kernel_size,
                                strides=strides, kernel_initializer='he_normal', name='DeConv'+str(n_filters))
@@ -183,7 +224,11 @@ def get_up_convolution(n_filters, strided_conv_size, kernel_size=(2, 2, 2), stri
     
 def create_dilated_fusion_block(input_layer, n_filters, layer_depth=2, dilation_depth=3, batch_normalization=False, kernel=(3, 3, 3), activation=None,
                              padding='same', strides=(1, 1, 1), instance_normalization=False):
-    # Create dilated blocks depending on depth
+    '''
+    :param layer_depth: Number denoting which depth-level to construct dilated fusion block in, used to name layers in later function (should always be specified, but default is 2)
+    :param dilation_depth: Number determining length of dilated_fusion_block (default is 3). Increases dilation rate (in increments of 2**('dilation_depth')) for each additional dilation depth
+    '''
+    # Create dilated blocks depending on depth. dil_rate goes: 1 --> 2 --> 4 --> 8 etc.
     for dil_rate in range(dilation_depth):
         if dil_rate == 0:
             layer = dilated_couple(input_layer, n_filters, layer_depth, kernel=kernel, padding=padding, strides=strides, dilation_rate=2**dil_rate, dropout=0.5)
@@ -195,9 +240,11 @@ def create_dilated_fusion_block(input_layer, n_filters, layer_depth=2, dilation_
 
 # Consider SpatialDropout3D instead of Dropout, if performance is bad, especially good in early layers
 # Give input layer and outputs concatenated layer of two sets of two dil convs.
-# NOTE - Can be mdke neater with for loop
+# NOTE - Can be made neater with for loop
+# NOTE - There is a LOT of Dropout here. Additionally a bug has been reported where dropout was not properly applied. Check here if regularization is bonkers
 def dilated_couple(input_layer, n_filters, layer_depth, dilation_rate=1, batch_normalization=False, kernel=(3, 3, 3), activation=None,
                              padding='same', strides=(1, 1, 1), instance_normalization=False, dropout = 0.0):
+ 
     # First conv has kernel (1,1,1) to reduce feature maps and thereby computational load
     layer = dilated_conv(input_layer, n_filters, kernel = (1,1,1), padding=padding, strides=strides, dilation_rate=dilation_rate, name = str(layer_depth)+'dil_rate_'+str(dilation_rate)+'_'+str(n_filters)+'_1')
     # filters should be the same as base layer here
@@ -211,6 +258,7 @@ def dilated_couple(input_layer, n_filters, layer_depth, dilation_rate=1, batch_n
     layer = dilated_conv(layer, n_filters//(2**2), kernel=kernel, padding=padding, strides=strides, dilation_rate=dilation_rate, name = str(layer_depth)+'dil_rate_'+str(dilation_rate)+'_'+str(n_filters//(2**2))+'_2')
     
     layer = Dropout(dropout)(layer)
+    
     # Final layer is concatenated output of first 2 dil convs and second set of 2 dil convs
     final_layer = concatenate([layer, concat], axis=1)
     return final_layer
