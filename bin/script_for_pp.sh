97dcbfeadb330df68991f558bf72e70611037c4e
thomshaw92 <thomasbasilshaw@gmail.com>
2019-09-26 12:05:40 +1000

    fixed it up

--- bin/script_for_pp.sh
+++ bin/script_for_pp.sh
@@ -11,91 +11,32 @@ workdir=/data/fastertemp/uqtshaw/DEEPSEACAT_atlas
 mkdir ${workdir}
 cd ${workdir}
 
-
-#then average all the images in one of the datasets (that'll do, don't need all of them for an initial template)
+# average all the images in one of the datasets (that'll do, don't need all of them for an initial template)
 for seg in mprage tse ; do
     for side in left right ; do
 	AverageImages 3 ${workdir}/${side}_averaged_${seg}.nii.gz 1 ../ashs_atlas_umcutrecht_7t_20170810/train/train*/${seg}_to_chunktemp_${side}.nii.gz
-
-	#fix up the size and pad
-
+ 	#fix up the size (this isn't needed but whatever)
 	c3d ${workdir}/${side}_averaged_${seg}.nii.gz \
 	    -type float \
 	    -resample-mm 0.35x0.35x0.35mm \
-	    -pad-to 176x144x128 0 \
 	    -interpolation sinc \
 	    -o ${workdir}/${side}_averaged_${seg}_resampled-0.35mmIso.nii.gz
-
-    done
-done
-
-#Then create the template 
-#first the files to a CSV 
-for side in left right ; do
-    for x in {00..32} ; do
-	echo "/data/fastertemp/uqtshaw/ashs_atlas_magdeburg_7t_20180416/train/train0${x}/tse_to_chunktemp_${side}.nii.gz,/data/fastertemp/uqtshaw/ashs_atlas_magdeburg_7t_20180416/train/train0${x}/mprage_to_chunktemp_${side}.nii.gz">>${side}_template_input.csv
-    done
-    for x in  {00..25} ; do
-	echo "/data/fastertemp/uqtshaw/ashs_atlas_umcutrecht_7t_20170810/train/train0${x}/tse_to_chunktemp_${side}.nii.gz,/data/fastertemp/uqtshaw/ashs_atlas_umcutrecht_7t_20170810/train/train0${x}/mprage_to_chunktemp_${side}.nii.gz">>${side}_template_input.csv
-    done
-done
-
-#make a template of the average morphology. Prolly only need one iteration as they are already all registered. 
-for side in left right ; do
-    mkdir ${workdir}/${side}_template
-    cd ${workdir}/${side}_template
-    antsMultivariateTemplateConstruction2.sh -d 3 \
-					     -i 3 \
-					     -g 0.20 \
-					     -k 2 \
-					     -t SyN \
-					     -n 0 \
-					     -z ${workdir}/${side}_averaged_tse_resampled-0.35mmIso.nii.gz \
-					     -z ${workdir}/${side}_averaged_mprage_resampled-0.35mmIso.nii.gz \
-					     -m CC \
-					     -c 5 \
-					     -y 1 \
-					     -o ${side}_ ${workdir}/${side}_template_input.csv 
-	cd ${workdir}
-	cp ${workdir}/${side}_template/${side}_template0.nii.gz ${workdir}/${side}_tse_template_resampled-0.35mmIso_padded_176x144x128.nii.gz
-	cp ${workdir}/${side}_template/${side}_template1.nii.gz ${workdir}/${side}_mprage_template_resampled-0.35mmIso_padded_176x144x128.nii.gz
-done
-
-#Rescale intensities between -1 and 1
-for seg in mprage tse ; do
-    for side in left right ; do
+	#Rescale intensities between -1 and 1
 	ImageMath 3 \
-		   ${workdir}/${side}_${seg}_template_resampled-0.35mmIso_padded_176x144x128_rescaled.nii.gz \
+		  ${workdir}/${side}_${seg}_template_resampled-0.35mmIso_rescaled.nii.gz \
 		  RescaleImage -1 1 \
-		   ${workdir}/${side}_${seg}_template_resampled-0.35mmIso_padded_176x144x128.nii.gz
-    done
-done
-
-#Give the templates zero mean and unit variance.
-for seg in mprage tse ; do
-    for side in left right ; do
+		  ${workdir}/${side}_${seg}_template_resampled-0.35mmIso.nii.gz
+	#Give the templates zero mean and unit variance.
 	std=""
 	mean=""
 	mean=`fslstats ${x} -m | awk '{print $1}'`
-	fslmaths ${workdir}/${side}_${seg}_template_resampled-0.35mmIso_padded_176x144x128_rescaled.nii.gz -sub ${mean} ${workdir}/${side}_${seg}_template_resampled-0.35mmIso_padded_176x144x128_rescaled_0mean.nii.gz
+	fslmaths ${workdir}/${side}_${seg}_template_resampled-0.35mmIso_rescaled.nii.gz -sub ${mean} ${workdir}/${side}_${seg}_template_resampled-0.35mmIso_rescaled_0mean.nii.gz
 	std=`fslstats ${side}_${seg}_template_resampled-0.35mmIso_padded_176x144x128_rescaled_0mean.nii.gz -s | awk '{print $1}'`
-	fslmaths ${workdir}/${side}_${seg}_template_resampled-0.35mmIso_padded_176x144x128_rescaled_0mean.nii.gz -div ${std} ${workdir}/${side}_${seg}_template_resampled-0.35mmIso_padded_176x144x128_rescaled_0meanUv.nii.gz
+	fslmaths ${workdir}/${side}_${seg}_template_resampled-0.35mmIso_rescaled_0mean.nii.gz -div ${std} ${workdir}/${side}_${seg}_template_resampled-0.35mmIso_rescaled_0meanUv.nii.gz
     done
 done
 
-#../ashs_atlas_umcutrecht_7t_20170810/train/train000/seg_right.nii.gz
-#antsRegistrationSyNQuick.sh -d 3 -t a -o tse_test_ants_to_chunk -f ./right_averaged_tse_resampled-0.35mmIso.nii.gz -m ../ashs_atlas_umcutrecht_7t_20170810/train/train000/tse.nii.gz
-#antsApplyTransforms -d 3 -o ${movingMaskWarped} -i ${movingMask} -r ${fixedT1} -n NearestNeighbor -t tse_test_ants_to_chunk1Warp.nii.gz -t ${outputPrefix}0GenericAffine.mat
 
-
-#ok, the mprage was in a completely different space to the segmentation, so we need to warp the mp2rage back to normal space
-#this code works to trasform the TSE to the space of the MPRAGE but i can't invert it for some reason. (because the inverse warp isn't included, yay.)
-#########
-antsApplyTransforms -d 3 -o ../ashs_atlas_umcutrecht_7t_20170810/train/train010/tse_native_chunk_right_to_mprage.nii.gz -i ../ashs_atlas_umcutrecht_7t_20170810/train/train010/tse_native_chunk_right.nii.gz -r ../ashs_atlas_umcutrecht_7t_20170810/train/train010/mprage_to_chunktemp_right.nii.gz -n BSpline -t ../ashs_atlas_umcutrecht_7t_20170810/train/train010/greedy_t1_to_template_right_warp.nii.gz  -t ../ashs_atlas_umcutrecht_7t_20170810/train/train010/ants_t1_to_tempAffine.txt
-###########
-#OK, step one. Get the MPRAGE to be in the same space as the TSE. We can initialise it with the inverse .mat and do a quickSyn to get it near perfectly aligned with the TSE.
-#there is probably no other solution unless we can invert thedeformation field..
-#nope that doesn't exist
 
 ##############################################################
 # writing the rest of the code in bash to show as an example #
@@ -108,64 +49,85 @@ antsApplyTransforms -d 3 -o ../ashs_atlas_umcutrecht_7t_20170810/train/train010/
 ##############
 
 #make a directory for the final preprocessed data
-mkdir ${workdir}/preprocessing_inputs
+mkdir ${workdir}/preprocessing_output
 
-#flirt the mprage to the template using sqform. I'll just do the Mag dataset
-atlas_dir=/data/fastertemp/uqtshaw/ashs_atlas_magdeburg_7t_20180416/train
-
+#resize the TSE
 for side in left right ; do
     for x in {00..32} ; do
-	flirt -in ${atlas_dir}/train0${x}/mprage_to_chunktemp_${side}.nii.gz \
-	      -applyxfm \
-	      -usesqform \
-	      -interp sinc \
-	      -ref ${workdir}/${side}_mprage_template_resampled-0.35mmIso_padded_176x144x128_rescaled_0meanUv.nii.gz \
-	      -out ${workdir}/preprocessing_inputs/mag_${x}_mprage_resampled-0.35mmIso_padded_176x144x128.nii.gz
-	#Step Two
-	#do the TSE
-	flirt -in ${atlas_dir}/train0${x}/tse.nii.gz \
-	      -applyxfm \
-	      -usesqform \
-	      -interp sinc \
-	      -ref ${workdir}/${side}_tse_template_resampled-0.35mmIso_padded_176x144x128_rescaled_0meanUv.nii.gz \
-	      -out ${workdir}/preprocessing_inputs/mag_${x}_tse_resampled-0.35mmIso_padded_176x144x128.nii.gz
-	#Step Three
-	#Do the Segmentation
-	flirt -in ${atlas_dir}/train0${x}/tse.nii.gz \
-	      -applyxfm \
-	      -usesqform \
-	      -interp nearestneighbour \
-	      -ref ${workdir}/${side}_tse_template_resampled-0.35mmIso_padded_176x144x128_rescaled_0meanUv.nii.gz \
-	      -out ${workdir}/preprocessing_inputs/mag_${x}_seg_resampled-0.35mmIso_padded_176x144x128.nii.gz
+	#fix the size of the TSE and seg
+	c3d ${atlas_dir}/train0${x}/tse_native_chunk_${side}.nii.gz \
+	    -type float \
+	    -resample-mm 0.35x0.35x0.35mm \
+	    -interpolation sinc \
+	    -o ${workdir}/${x}_${side}_tse_chunk_resampled-0.35mmIso.nii.gz
+	c3d ${atlas_dir}/train0${x}/tse_native_chunk_${side}_seg.nii.gz \
+	    -type float \
+	    -resample-mm 0.35x0.35x0.35mm \
+	    -interpolation sinc \
+	    -o ${workdir}/${x}_${side}_seg_chunk_resampled-0.35mmIso.nii.gz
+	
+	##############
+
+	## Step Two ##
+
+	##############
+	
+	#ants the mprage to the tse. I'll just do the Mag dataset for the moment, the affine doesn't exist for the mag dataset (yay) so i'll let ants do all the work
+	
+	atlas_dir=/data/fastertemp/uqtshaw/ashs_atlas_magdeburg_7t_20180416/train
+	mprage=${atlas_dir}/train0${x}/mprage_to_chunktemp_${side}.nii.gz
+	tse=${workdir}/${side}_tse_native_chunk_${side}_resampled-0.35mmIso.nii.gz
+
+	antsRegistrationantsRegistration --dimensionality 3 --float 0 \  
+        --output [${workdir}/${x}_${side}_mprage_chunk_resampled-0.35mmIso_,${workdir}/${x}_${side}_mprage_chunk_resampled-0.35mmIsoWarped.nii.gz] \  
+        --interpolation BSpline \  
+        --use-histogram-matching 0 \  
+        --transform Rigid[0.15] \  
+        --metric MI[${tse},${mprage},1,32,Regular,0.25] \  
+        --convergence [1000x500x250x100,1e-6,10] \  
+        --shrink-factors 8x4x2x1 \  
+        --smoothing-sigmas 3x2x1x0vox \  
+        --metric MI[${tse},${mprage},1,32,Regular,0.25] \  
+        --convergence [1000x500x250x100,1e-6,10] \  
+        --shrink-factors 8x4x2x1 \  
+        --smoothing-sigmas 3x2x1x0vox \    
+        --transform SyN[0.1,3,0] \  
+        --metric MI[${tse},${mprage},1,32,Regular,0.25] \  
+        --convergence [100x70x50x20,1e-6,10] \  
+        --shrink-factors 8x4x2x1 \  
+        --smoothing-sigmas 3x2x1x0vox \  
+
+	##############
+
+	##  Step 3  ##
+
+	##############
+	#pad the TSE, MPRAGE, and segmentation 
+	for seg in tse mprage seg ; do
+	    c3d ${workdir}/${x}_${side}_${seg}_chunk_resampled-0.35mmIso*.nii.gz  \
+		-type float \
+		-pad-to 176x144x128 0 \
+		-interpolation sinc \
+		-o ${workdir}/${x}_${side}_${seg}_chunk_resampled-0.35mmIso_pad-176x144x128.nii.gz
+
+	    ##############
+
+	    ##  Step 4  ##
+
+	    ##############
+	    #normalise everything  to template
+	    
+	    c3d ${workdir}/${side}_${seg}_template_resampled-0.35mmIso_rescaled_0meanUv.nii.gz \
+		${workdir}/${x}_${side}_${seg}_chunk_resampled-0.35mmIso_pad-176x144x128.nii.gz \
+		-histmatch 5 \
+		-type float \
+		-interpolation sinc \
+		-o ${workdir}/${x}_${side}_${seg}_chunk_resampled-0.35mmIso_pad-176x144x128_norm.nii.gz 
+	    
+	    #normalise everything to the template
+	done
     done
 done
-
-
-##############
-
-##  Step 4  ##
-
-##############
-#normalise everything to the template
-
-c3d 
-
-normalise_mprage_n = MapNode(c3d(histmatch=True,
-
-                  name='normalise_mprage_n', iterfield=['in_file'])
-
-wf.connect([(selectfiles, mprage_flirt_n, [('out_file', 'in_file')])]) #check that this works FIXME
-
-#include the reference mprage here
-
-normalise_tse_n = MapNode(c3d(histmatch=True,
-
-                  name='normalise_tse_n', iterfield=['in_file'])
-
-wf.connect([(selectfiles, tse_flirt_n, [('out_file', 'in_file')])])
-
-wf.connect #need to include the reference file here
-
 
 
 
