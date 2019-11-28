e8a475e3b587d69c2b4cfbda1b00e66c15e2cdd7
uqmtottr <uqmtottr@wiener.hpc.dc.uq.edu.au>
2019-11-29 08:51:59 +1000

    Boxplot created on normalized volume data

--- CAI-Unet/volume_deviation_and_boxplot.py
+++ CAI-Unet/volume_deviation_and_boxplot.py
@@ -13,7 +13,7 @@ from glob import glob
 import matplotlib.pyplot as plt
 import numpy as np
 import nibabel as nib
-
+import scipy.stats as stat
 
 data_path = glob('/scratch/cai/DEEPSEACAT/data/20191107_leaky_ReLu/test_prediction/time_test/validation*/truth.nii.gz')
 pred_path = glob('/scratch/cai/DEEPSEACAT/data/20191107_leaky_ReLu/test_prediction/time_test/CC/*/*.nii.gz')
@@ -136,27 +136,104 @@ dev_percentage5 = (vol_dev5/l5_volume)*100
 dev_percentage6 = (vol_dev6/l6_volume)*100
 dev_percentage8 = (vol_dev8/l8_volume)*100
 
+
+
+# Normalization of volume data
+# Manual segmentations
+sub_sum = []
+norm1 = []
+norm2 = []
+norm3 = []
+norm4 = []
+norm5 = []
+norm6 = []
+norm8 = []
+
+for i in range(11):
+    sub_sum = l1[i]+l2[i]+l3[i]+l4[i]+l5[i]+l6[i]+l8[i]
+    norm1.append(l1[i]/sub_sum)
+    norm2.append(l2[i]/sub_sum)
+    norm3.append(l3[i]/sub_sum)
+    norm4.append(l4[i]/sub_sum)
+    norm5.append(l5[i]/sub_sum)
+    norm6.append(l6[i]/sub_sum)
+    norm8.append(l8[i]/sub_sum)
+
+norm1_avg = np.mean(norm1)
+norm2_avg = np.mean(norm2)
+norm3_avg = np.mean(norm3)
+norm4_avg = np.mean(norm4)
+norm5_avg = np.mean(norm5)
+norm6_avg = np.mean(norm6)
+norm8_avg = np.mean(norm8)
+
+norm1_std = np.std(norm1)
+norm2_std = np.std(norm2)
+norm3_std = np.std(norm3)
+norm4_std = np.std(norm4)
+norm5_std = np.std(norm5)
+norm6_std = np.std(norm6)
+norm8_std = np.std(norm8)
+
+norm_manual = [norm1, norm2, norm3, norm4, norm5, norm6, norm8]
+norm_manual = np.reshape(norm_manual,[7,11])
+norm_manual = norm_manual.transpose()
+np.save('/clusterdata/uqmtottr/DEEPSEACAT-Deep-learning-for-Segmentation-And-CharActerisation-of-Tissue/CAI-Unet/norm_manual', norm_manual)
+
+# Predicted segmentations
+sub_sum_pred = []
+norm1_pred = []
+norm2_pred = []
+norm3_pred = []
+norm4_pred = []
+norm5_pred = []
+norm6_pred = []
+norm8_pred = []
+
+for i in range(11):
+    sub_sum_pred = p1[i]+p2[i]+p3[i]+p4[i]+p5[i]+p6[i]+p8[i]
+    norm1_pred.append(p1[i]/sub_sum_pred)
+    norm2_pred.append(p2[i]/sub_sum_pred)
+    norm3_pred.append(p3[i]/sub_sum_pred)
+    norm4_pred.append(p4[i]/sub_sum_pred)
+    norm5_pred.append(p5[i]/sub_sum_pred)
+    norm6_pred.append(p6[i]/sub_sum_pred)
+    norm8_pred.append(p8[i]/sub_sum_pred)
+
+norm1_avg_pred = np.mean(norm1_pred)
+norm2_avg_pred = np.mean(norm2_pred)
+norm3_avg_pred = np.mean(norm3_pred)
+norm4_avg_pred = np.mean(norm4_pred)
+norm5_avg_pred = np.mean(norm5_pred)
+norm6_avg_pred = np.mean(norm6_pred)
+norm8_avg_pred = np.mean(norm8_pred)
+
+norm1_std_pred = np.std(norm1_pred)
+norm2_std_pred = np.std(norm2_pred)
+norm3_std_pred = np.std(norm3_pred)
+norm4_std_pred = np.std(norm4_pred)
+norm5_std_pred = np.std(norm5_pred)
+norm6_std_pred = np.std(norm6_pred)
+norm8_std_pred = np.std(norm8_pred)
+
+norm_pred = [norm1_pred, norm2_pred, norm3_pred, norm4_pred, norm5_pred, norm6_pred, norm8_pred]
+norm_pred = np.reshape(norm_pred,[7,11])
+norm_pred = norm_pred.transpose()
+np.save('/clusterdata/uqmtottr/DEEPSEACAT-Deep-learning-for-Segmentation-And-CharActerisation-of-Tissue/CAI-Unet/norm_pred', norm_pred)
+
+
+### T-test calculation ###
+ttest = stat.ttest_ind(norm_manual, norm_pred)
+
 ### BOXPLOT ###
-tot_vol_true = [l1,l2,l3,l4,l5,l6,l8]
-tot_true = np.reshape(tot_vol_true,[7,11])
-tot_true = tot_true.transpose()
-
-tot_vol_pred = [p1,p2,p3,p4,p5,p6,p8]
-tot_pred = np.reshape(tot_vol_pred,[7,11])
-tot_pred = tot_pred.transpose()
-
-dice_label_array = tot_pred*0.35**3
-dice_label1_array = tot_true*0.35**3
-
-
-box_plot_data=[dice_label_array[:,0], dice_label_array[:,1], dice_label_array[:,2], dice_label_array[:,3], dice_label_array[:,4], dice_label_array[:,5], dice_label_array[:,6], np.mean(dice_label_array,axis=0)]
-box_plot_data1=[dice_label1_array[:,0], dice_label1_array[:,1], dice_label1_array[:,2], dice_label1_array[:,3], dice_label1_array[:,4], dice_label1_array[:,5], dice_label1_array[:,6], np.mean(dice_label1_array,axis=0)]
-box=plt.boxplot(box_plot_data, labels=['ERC', 'Sub','CA1','CA2', 'DG', 'CA3', 'Tail', 'Mean'], patch_artist=True, showfliers=False, positions=np.array(range(len(box_plot_data)))*2.0-0.4, widths = 0.5)
+box_plot_data=[norm_pred[:,0], norm_pred[:,1], norm_pred[:,2], norm_pred[:,3], norm_pred[:,4], norm_pred[:,5], norm_pred[:,6], np.mean(norm_pred,axis=0)]
+box_plot_data1=[norm_manual[:,0], norm_manual[:,1], norm_manual[:,2], norm_manual[:,3], norm_manual[:,4], norm_manual[:,5], norm_manual[:,6], np.mean(norm_manual,axis=0)]
+box=plt.boxplot(box_plot_data, labels=['ERC', 'Sub','CA1','CA2$^a$', 'DG', 'CA3', 'Tail', 'Mean'], patch_artist=True, showfliers=False, positions=np.array(range(len(box_plot_data)))*2.0-0.4, widths = 0.5)
 box1=plt.boxplot(box_plot_data1, labels=['', '','','', '', '', '', ''], patch_artist=True, showfliers=False, positions=np.array(range(len(box_plot_data1)))*2.0+0.2, widths = 0.5)
 plt.title('Volume for Proposed and Manual Method')
-plt.ylabel('MM3')
-colors1 = ['#e41a1c', '#e41a1c', '#e41a1c', '#e41a1c', '#e41a1c', '#e41a1c', '#e41a1c', '#e41a1c'] #["#1b9e77", "#d95f02", "#7570b3", "#e7298a", "#66a61e", "#e6ab02", "#a6761d", "#666666"]
-colors = ['#377eb8', '#377eb8', '#377eb8', '#377eb8', '#377eb8', '#377eb8', '#377eb8', '#377eb8'] #["#25daa4", "#fd8c35", "#9e9bca", "#f28cc1", "#85d728", "#fdd568", "#c38b22", "#999999"] 
+plt.ylabel('Normalized volume')
+colors = ['#984ea3']*8
+colors1 = ['#df65b0']*8
 for patch, color in zip(box['boxes'], colors):
     patch.set_facecolor(color)
 for median in box['medians']:
@@ -166,4 +243,5 @@ for patch, color in zip(box1['boxes'], colors1):
 for median in box1['medians']:
     median.set(color='black')
 plt.legend([box["boxes"][0], box1["boxes"][0]], ['Proposed method', 'Manual method'], loc='upper right')
+plt.savefig('/scratch/cai/DEEPSEACAT/data/Boxplot_vol.eps', format = 'eps')
 plt.show()
