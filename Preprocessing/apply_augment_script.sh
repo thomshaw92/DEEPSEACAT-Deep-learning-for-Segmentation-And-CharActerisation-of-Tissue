4f54ed62b448145e65be3a49b1100c6539b0a650
thomshaw92 <thomasbasilshaw@gmail.com>
2020-10-03 23:19:26 +1000

    fixes for Francesco

--- Preprocessing/apply_augment_script.sh
+++ Preprocessing/apply_augment_script.sh
@@ -3,76 +3,78 @@
 #will make 100 random augmented hippo things then apply warps to mprage tse and seg
 #then move the whole thing to new dataset 
 #dataset location
-dataset=/30days/uqtshaw/DEEPSEACAT/data_config_flipped
-#have to do it twice because my brain wont work out how to keep the count the same.
-count=61
-for participant in {000..025} ; do
-    cd ${dataset}/${participant}_left
-    cp seg_left.nii.gz mprage_left_seg.nii.gz
-    cp tse_left.nii.gz mprage_left_tse.nii.gz
-    python /30days/uqtshaw/DEEPSEACAT/augment_hippocampus.py mprage_left.nii.gz
-    rm mprage_left_tse.nii.gz mprage_left_seg.nii.gz
-    #move loop for new segmentations
-    for x in {0000..0099} ; do
-        mkdir ../0${count}_left
-        mv ./generated/g_mprage_left_v${x}.nii.gz ../0${count}_left/mprage_left.nii.gz
-        mv ./generated/g_mprage_left_v${x}_labels.nii.gz ../0${count}_left/seg_left.nii.gz
-        mv ./generated/g_mprage_left_v${x}_tse.nii.gz ../0${count}_left/tse_left.nii.gz
-        let "count++"
+dataset=/30days/uqtshaw/DEEPSEACAT/data_vikings
+#Do it for every set of participants
+#make sure you rename the mag_participants first and then make 
+
+script=/30days/uqtshaw/DEEPSEACAT/DEEPSEACAT-Deep-learning-for-Segmentation-And-CharActerisation-of-Tissue/Preprocessing/augment_hippocampus.py
+
+for side in left right ; do 
+    count=0
+    #TEST
+    for participant in '006' '011' '017' '023' '031' '042' ; do
+	cd ${dataset}/${participant}_${side}
+	cp seg_${side}.nii.gz mprage_${side}_seg.nii.gz
+	cp tse_${side}.nii.gz mprage_${side}_tse.nii.gz
+	python ${script} mprage_${side}.nii.gz
+	rm mprage_${side}_tse.nii.gz mprage_${side}_seg.nii.gz
+	#move loop for new segmentations
+	for x in {0000..0099} ; do
+            mkdir -p ../../TEST/0${count}_${side}
+            mv ./generated/g_mprage_${side}_v${x}.nii.gz ../../TEST/0${count}_${side}/mprage_${side}.nii.gz
+            mv ./generated/g_mprage_${side}_v${x}_labels.nii.gz ../../TEST/0${count}_${side}/seg_${side}.nii.gz
+            mv ./generated/g_mprage_${side}_v${x}_tse.nii.gz ../../TEST/0${count}_${side}/tse_${side}.nii.gz
+            let "count++"
+	done
+	rm -r ./generated
     done
-    rm -r ./generated
+done
+#validate
+for side in left right ; do 
+    count=0
+    for participant in '003' '007' '013' '019' '029' '038'; do
+	cd ${dataset}/${participant}_${side}
+	cp seg_${side}.nii.gz mprage_${side}_seg.nii.gz
+	cp tse_${side}.nii.gz mprage_${side}_tse.nii.gz
+	python  ${script} mprage_${side}.nii.gz
+	rm mprage_${side}_tse.nii.gz mprage_${side}_seg.nii.gz
+	#move loop for new segmentations
+	for x in {0000..0099} ; do
+            mkdir -p ../../VALIDATE/0${count}_${side}
+            mv ./generated/g_mprage_${side}_v${x}.nii.gz ../../VALIDATE/0${count}_${side}/mprage_${side}.nii.gz
+            mv ./generated/g_mprage_${side}_v${x}_labels.nii.gz ../../VALIDATE/0${count}_${side}/seg_${side}.nii.gz
+            mv ./generated/g_mprage_${side}_v${x}_tse.nii.gz ../../VALIDATE/0${count}_${side}/tse_${side}.nii.gz
+            let "count++"
+	done
+	rm -r ./generated
+    done
+done
+#TRAIN (include if statement that will increment count if the directory doesn't exist in order to keep the count consistent between missing data sets.)
+for side in left right ; do 
+    count=0
+    for participant in '001' '004' '009' '010' '012' '014' '020' '021' '022' '025' '028' '030' '033' '035' '040' '043' '048' '049' '050' '051' '052' '054' '055' '056' '057' '058' '044' '053' '000' '002' '005' '008' '015' '016' '018' '024' '026' '027' '034' '036' '037' '039' '046' '059' '047' ; do
+	if [[ -d ${dataset}/${participant}_${side} ]] ; then 
+	cd ${dataset}/${participant}_${side}
+	cp seg_${side}.nii.gz mprage_${side}_seg.nii.gz
+	cp tse_${side}.nii.gz mprage_${side}_tse.nii.gz
+	python  ${script} mprage_${side}.nii.gz
+	rm mprage_${side}_tse.nii.gz mprage_${side}_seg.nii.gz
+	#move loop for new segmentations
+	for x in {0000..0099} ; do
+            mkdir -p ../../TRAIN/0${count}_${side}
+            mv ./generated/g_mprage_${side}_v${x}.nii.gz ../../TRAIN/0${count}_${side}/mprage_${side}.nii.gz
+            mv ./generated/g_mprage_${side}_v${x}_labels.nii.gz ../../TRAIN/0${count}_${side}/seg_${side}.nii.gz
+            mv ./generated/g_mprage_${side}_v${x}_tse.nii.gz ../../TRAIN/0${count}_${side}/tse_${side}.nii.gz
+            let "count++"
+	done
+	rm -r ./generated
+	else
+	    for x in {0000..0099} ; do
+	       let "count++"	
+	    done
+	fi	
+    done
 done
 
-count=61
-for participant in {000..025} ; do
-    cd ${dataset}/${participant}_right
-    cp seg_right.nii.gz mprage_right_seg.nii.gz
-    cp tse_right.nii.gz mprage_right_tse.nii.gz
-    python /30days/uqtshaw/DEEPSEACAT/augment_hippocampus.py mprage_right.nii.gz 
-    rm mprage_right_tse.nii.gz mprage_right_seg.nii.gz
-    #move loop for new segmentations
-    for x in {0000..0099} ; do
-        mkdir ../00${count}_right
-        mv ./generated/g_mprage_right_v${x}.nii.gz ../00${count}_right/mprage_right.nii.gz
-        mv ./generated/g_mprage_right_v${x}_labels.nii.gz ../00${count}_right/seg_right.nii.gz
-        mv ./generated/g_mprage_right_v${x}_tse.nii.gz ../00${count}_right/tse_right.nii.gz
-        let "count++"
-    done
-    rm -r generated
-done
-#now do the mag ones
-#count is the number done so far (2600 + 60)
-count=2661
-for participant in {026..060} ; do
-    cd ${dataset}/${participant}_left
-    cp mag_seg_left.nii.gz mag_mprage_left_seg.nii.gz
-    cp mag_tse_left.nii.gz mag_mprage_left_tse.nii.gz
-    python /30days/uqtshaw/DEEPSEACAT/augment_hippocampus.py mag_mprage_left.nii.gz
-    rm mag_mprage_left_tse.nii.gz mag_mprage_left_seg.nii.gz
-    #move loop for new segmentations
-    for x in {0000..0099} ; do
-        mkdir ../0${count}_left
-        mv ./generated/g_mag_mprage_left_v${x}.nii.gz ../0${count}_left/mag_mprage_left.nii.gz
-        mv ./generated/g_mprage_left_v${x}_labels.nii.gz ../0${count}_left/mag_seg_left.nii.gz
-        mv ./generated/g_mprage_left_v${x}_tse.nii.gz ../0${count}_left/mag_tse_left.nii.gz
-        let "count++"
-    done
-    rm -r ./generated
-done
-count=2661
-for participant in {026..061} ; do
-    cd ${dataset}/${participant}_right
-    cp mag_seg_right.nii.gz mag_mprage_right_seg.nii.gz
-    cp mag_tse_right.nii.gz mag_mprage_right_tse.nii.gz
-    python /30days/uqtshaw/DEEPSEACAT/augment_hippocampus.py mprage_right.nii.gz
-    rm mprage_right_tse.nii.gz mprage_right_seg.nii.gz
-    #move loop for new segmentations
-    for x in {0000..0099} ; do
-        mkdir ../0${count}_right
-        mv ./generated/g_mprage_right_v${x}.nii.gz ../0${count}_right/mag_mprage_right.nii.gz
-        mv ./generated/g_mprage_right_v${x}_labels.nii.gz ../0${count}_right/mag_seg_right.nii.gz
-        mv ./generated/g_mprage_right_v${x}_tse.nii.gz ../0${count}_right/mag_tse_right.nii.gz
-        let "count++"
-    done
-    rm -r generated
-done
+#TRAIN incomplete (left or right only) datasets...
+
